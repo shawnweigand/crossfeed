145740a950a07ef016bc26dec09f8b2eab830c59
Shawn Weigand <shawnweigand1@gmail.com>
2024-12-05 07:30:54 -0500

    spotify service

--- app/Services/SpotifyService.php
+++ app/Services/SpotifyService.php
@@ -18,12 +18,13 @@ class SpotifyService
         $this->token_url = config('services.spotify.token_url');
         $this->base_url = config('services.spotify.base_url');
         $this->client_id = config('services.spotify.client_id');
-        $this->client_id = config('services.spotify.client_secret');
+        $this->client_secret = config('services.spotify.client_secret');
         $this->token = $this->getToken();
     }
 
     public function getToken() {
         $auth = base64_encode($this->client_id . ':' . $this->client_secret);
+
         $response = Http::asForm()->withHeaders([
             'Authorization' => 'Basic ' . $auth
         ])->post($this->token_url, [
@@ -31,12 +32,14 @@ class SpotifyService
         ]);
 
         if ($response->successful()) return $response->json()['access_token'];
+        else return $response->json();
     }
 
     public function search($term) {
         $endpoint = '/search';
         $url = $this->base_url . $endpoint;
         if (!$this->token) $this->getToken();
+
         $response = Http::withToken($this->token)
         ->withQueryParameters([
             'type' => 'show',
@@ -46,6 +49,21 @@ class SpotifyService
         ])->get($url);
 
         if ($response->successful()) return $response->json();
+        else return $response->json();
+    }
+
+    public function episodes($showId) {
+        $endpoint = "/shows/$showId/episodes";
+        $url = $this->base_url . $endpoint;
+        if (!$this->token) $this->getToken();
+
+        $response = Http::withToken($this->token)
+        ->withQueryParameters([
+            'limit' => 10
+        ])->get($url);
+
+        if ($response->successful()) return $response->json();
+        else return $response->json();
     }
 
 }
--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\SpotifyService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Http;
@@ -9,44 +10,18 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('spotify:token', function () {
-    $url = 'https://accounts.spotify.com/api/token';
-    $auth = base64_encode(env('SPOTIFY_CLIENT_ID') . ':' . env('SPOTIFY_CLIENT_SECRET'));
-    $response = Http::asForm()->withHeaders([
-        'Authorization' => 'Basic ' . $auth
-    ])->post($url, [
-        'grant_type' => 'client_credential'
-    ]);
-
-    if ($response->successful()) {
-        $body = $response->json();
-        $token = $body['access_token'];
-        // Use the token as needed
-        $this->comment("Access Token: $token");
-    } else {
-        // Handle the error
-        print_r([
-            'status' => $response->status(),
-            'message' => $response->json()
-        ]);
-    }
+    $spotify = new SpotifyService();
+    $this->comment(json_encode($spotify->getToken()));
 });
 
 Artisan::command('spotify:search {term}', function (string $term) {
-    $base_url = 'https://api.spotify.com/v1';
-    $endpoint = '/search';
-    $response = Http::withToken(
-        'BQC8YArzrqfGH4a-z5-Xfj8fQwkAphUK-nIySAO1xglVgrfxFDwlCwcp3ka04E9Ci98Eg5vhvureEC_HylApV-7jJa5tM6fwQvhfwDTCI3Qg1Y5Wciw'
-    )->withQueryParameters([
-        'type' => 'show',
-        'q' => $term,
-        'market' => 'US',
-        'limit' => 20
-    ])->get($base_url . $endpoint);
+    $spotify = new SpotifyService();
+    $this->comment(json_encode($spotify->search($term)));
+});
 
-    if ($response->successful()) {
-        $this->comment($response);
-    } else {
-        $this->comment("Error: " . $response->status());
-        print_r($response->json());
-    }
+Artisan::command('spotify:episode {term}', function (string $term) {
+    $spotify = new SpotifyService();
+    $show = $spotify->search($term)['shows']['items'][0];
+    $episodes = $spotify->episodes(($show['id']));
+    $this->comment(json_encode($episodes['items']));
 });
