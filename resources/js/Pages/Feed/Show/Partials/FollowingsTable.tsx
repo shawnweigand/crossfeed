e63ab3fb92ee6868dcd49884b3ea52a234a249ca
Shawn Weigand <shawnweigand1@gmail.com>
2024-12-22 06:20:42 -0800

    key switch

--- resources/js/Pages/Feed/Show/Partials/FollowingsTable.tsx
+++ resources/js/Pages/Feed/Show/Partials/FollowingsTable.tsx
@@ -14,6 +14,7 @@ const people = [
     {
         name: 'Youtube dog highlights',
         email: 'short description...',
+        id: 1,
         role: 'YouTube',
         imageUrl:
           'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -22,6 +23,7 @@ const people = [
     {
         name: 'Spotify podcast about dogs',
         email: 'short description...',
+        id: 2,
         role: 'Spotify',
         imageUrl:
           'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -30,6 +32,7 @@ const people = [
     {
       name: 'Other medium...',
       email: 'short description...',
+      id: 3,
       role: 'Other',
       imageUrl:
         'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -44,7 +47,7 @@ export default function FollowingsTable({}: Props) {
             <h1 className='text-xl mb-2 text-gray-500'>Following</h1>
             <ul role="list" className="divide-y divide-gray-100">
             {people.map((person) => (
-                <li key={person.email} className="flex justify-between gap-x-6 py-5">
+                <li key={person.id} className="flex justify-between gap-x-6 py-5">
                 <div className="flex min-w-0 gap-x-4">
                     <img alt="" src={person.imageUrl} className="size-12 flex-none rounded-full bg-gray-50" />
                     <div className="min-w-0 flex-auto">
