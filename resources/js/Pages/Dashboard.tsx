338d59f75ec068efea140a9f22fa9d6298047c83
Shawn Weigand <shawnweigand1@gmail.com>
2025-01-02 06:54:37 -0500

    selecteed feed in dashboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -26,7 +26,7 @@ export default function Dashboard({feeds}: Props) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            You're logged in!
+                            Selected: {feeds.find(feed => feed.selected).name}
                         </div>
                     </div>
                 </div>
